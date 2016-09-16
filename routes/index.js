83633902e5b880985accec55dfb7923239e8bd05
Alex Horton <alexhortonmusic@gmail.com>
2016-09-16 14:26:31 -0500

    working vote buttons

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const router = Router()
 const newPost = require('../models/newPost')
 
 // models
-
 router.get('/', (req, res, err) =>
   newPost
     .find()
@@ -15,6 +14,35 @@ router.get('/', (req, res, err) =>
     )
     .catch(err)
 )
+
+// router.post("/:id/up", (req, res) => {
+//   let postID = req.params.id;
+//   Post.findById(postID, (err, docs) => {
+//     docs.votes++;
+//     docs.save((err) => {
+//       if(err)
+//       res.redirect("/");
+//     })
+//   });
+// });
+
+// up vote
+router.post('/:id/up', (req, res, err) => {
+  let postID = req.params.id;
+  newPost
+    .findByIdAndUpdate(postID, { $inc: {score: 1} })
+    .then(() => res.redirect('/'))
+    .catch(console.error)
+})
+
+//down vote
+router.post('/:id/down', (req, res, err) => {
+  let postID = req.params.id;
+  newPost
+    .findByIdAndUpdate(postID, { $inc: {score: -1} })
+    .then(() => res.redirect('/'))
+    .catch(console.error)
+})
 
 router.get('/new', (req, res) =>
 	res.render('new-topic')
